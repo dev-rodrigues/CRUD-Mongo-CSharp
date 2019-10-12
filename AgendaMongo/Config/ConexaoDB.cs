9215b54d68a9994b81bd0e85499633de5aad524c
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-12 00:36:43 -0300

    configurando conexao

---
+++ AgendaMongo/Config/ConexaoDB.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMongo.Config
+{
+    class ConexaoDB : IDisposable
+    {
+
+        public IMongoClient MongoClient { get; }
+        public IMongoCollection<BsonDocument> MongoCollection { get; }
+
+        public ConexaoDB()
+        {
+            try
+            {
+                var host = ConfigurationManager.AppSettings [ "host" ];
+                var databaseName = ConfigurationManager.AppSettings [ "database" ];
+                var collectionName = ConfigurationManager.AppSettings [ "collection" ];
+
+                MongoClient = new MongoClient( host );
+                IMongoDatabase database = MongoClient.GetDatabase( databaseName );
+                MongoCollection = database.GetCollection<BsonDocument>( collectionName );
+            } catch (Exception e)
+            {
+                Console.WriteLine( e.Message );
+            }
+
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize( this );
+        }
+    }
+}
