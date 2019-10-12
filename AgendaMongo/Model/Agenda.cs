77906b54c7d7c21f466b7e6975e2d3f69c44e360
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-12 00:12:02 -0300

    def agenda

---
+++ AgendaMongo/Model/Agenda.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMongo.Model
+{
+    class Agenda
+    {
+        public ObjectId Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public List<Contato> Contatos { get; set; }
+    }
+}
