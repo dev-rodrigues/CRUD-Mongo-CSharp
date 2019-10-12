60f10849548dca8c030d4c732d6e20bcb10ebecf
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-12 00:26:50 -0300

    interface

---
+++ AgendaMongo/Interface/IAgendaDAO.cs
@@ -0,0 +1,19 @@
+﻿using AgendaMongo.Model;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMongo.Interface
+{
+    interface IAgendaDAO
+    {
+        void Salvar( Agenda agenda );
+        void Atualizar( Agenda agenda );
+        void Deletar( ObjectId id );
+        List<Agenda> BuscaAgendas();
+        Agenda BuscaAgenda( String codigo );
+    }
+}
